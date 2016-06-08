717e0db25b25e5d09c7842a94c28e94c59419191
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-06-08 15:02:34 +0200

    wip

--- src/editors.js
+++ src/editors.js
@@ -1,12 +1,10 @@
+import { assign } from 'lodash';
 import BasicEditor from './components/editors/basic';
 
 export const editors = {};
 
 export const registerEditor = (name, component) => {
-    editors[name] = {
-        ...component,
-        props: ['media'],
-    };
+    editors[name] = assign({}, component, { props: ['media'] });
 };
 
 registerEditor('basic', BasicEditor);
